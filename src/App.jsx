ac363ed097e635f03cc37906e5ef05a98a4caf96
Rashmin47 <rashminxperience@gmail.com>
2025-08-19 17:54:12 +0545

    Finished

--- src/App.jsx
+++ src/App.jsx
@@ -36,7 +36,7 @@ function App() {
   }, [length, numberAllowed, charAllowed]);
 
   return (
-    <div className="w-full max-w-md mx auto shadow-md rounded-lg px-4 py-3 my-8 bg-gray-800 text-orange-500">
+    <div className="w-full  max-w-md mx-auto shadow-md rounded-lg px-4 py-3 my-8 bg-gray-800 text-orange-500">
       <h1 className="text-3xl text-center font-bold mb-2">
         Password Generator
       </h1>
